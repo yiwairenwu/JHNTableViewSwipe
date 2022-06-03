2c2d11307c17f277e300c916cbbec44c30bf5f32
JHN <smiler.coco@gmail.com>
2022-06-04 04:46:31 +0800

    1.增加上下文精度设置

--- JHNTableViewSwipe/Classes/UITableView+JHNSwipe.m
+++ JHNTableViewSwipe/Classes/UITableView+JHNSwipe.m
@@ -21,12 +21,12 @@
 @implementation UITableView(JHNSwipe)
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
 - (UIView *)swipeCellBg{
     return objc_getAssociatedObject(self,  @selector(swipeCellBg));
 }
@@ -93,8 +93,8 @@
     [self setCallBack:callBack];
     [self setShowsVerticalScrollIndicator:NO];
     UISwipeGestureRecognizer * recognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(handleSwipeFrom:)];
-   [recognizer setDirection:(UISwipeGestureRecognizerDirectionLeft)];
-   [self addGestureRecognizer:recognizer];
+    [recognizer setDirection:(UISwipeGestureRecognizerDirectionLeft)];
+    [self addGestureRecognizer:recognizer];
     [self setSwipe:recognizer];
 }
 - (void)addSectionCanSwipe:(bool_something_number)callBack{
@@ -102,90 +102,89 @@
 }
 - (void)handleSwipeFrom:(UISwipeGestureRecognizer *)recognizer{
     
-   
-   if(recognizer.direction == UISwipeGestureRecognizerDirectionLeft&&!self.isOpen.boolValue) {
-       CGPoint point = [recognizer locationInView:self];
-       if ([self.layer containsPoint:point]) {
+    
+    if(recognizer.direction == UISwipeGestureRecognizerDirectionLeft&&!self.isOpen.boolValue) {
+        CGPoint point = [recognizer locationInView:self];
+        if ([self.layer containsPoint:point]) {
+            
+            NSIndexPath *indexPath =[self indexPathForRowAtPoint:point];
+            if (self.boolBack) {
+                BOOL flag = self.boolBack(indexPath.section);
+                if (!flag) {
+                    return;
+                }
+            }
+            self.isOpen = [NSNumber numberWithInt:1];
+            CGRect rect = [self rectForSection:indexPath.section];
+            CGRect headRect = [self rectForHeaderInSection:indexPath.section];
+            CGRect footRect = [self rectForFooterInSection:indexPath.section];
+            CGRect supperRect = [self convertRect:rect toView:self.superview];
            
-           NSIndexPath *indexPath =[self indexPathForRowAtPoint:point];
-           if (self.boolBack) {
-               BOOL flag = self.boolBack(indexPath.section);
-               if (!flag) {
-                   return;
-               }
-           }
-           self.isOpen = [NSNumber numberWithInt:1];
-           CGRect rect = [self rectForSection:indexPath.section];
-           CGRect headRect = [self rectForHeaderInSection:indexPath.section];
-           CGRect footRect = [self rectForFooterInSection:indexPath.section];
-           CGRect supperRect = [self convertRect:rect toView:self.superview];
-//           CGFloat scale = [UIScreen mainScreen].scale;
-//           UIImage *image= [self.superview ClipToImageWithCGRect:CGRectMake(supperRect.origin.x, supperRect.origin.y<0?headRect.size.height: supperRect.origin.y+headRect.size.height, supperRect.size.width, supperRect.size.height-footRect.size.height+(supperRect.origin.y<0?supperRect.origin.y-headRect.size.height:-headRect.size.height))];
-           UIImage *image = [self screenShotForIndexPaths:indexPath];
-           UIView *view = [[UIView alloc] init];
-           [view setBackgroundColor:self.backgroundColor];
-           UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-           [view setFrame:CGRectMake(0,rect.origin.y+headRect.size.height, rect.size.width, image.size.height)];
-           [view setClipsToBounds:YES];
-         
-           
-//           [imageView setFrame:CGRectMake(0, rect.origin.y, image.size.width, image.size.height)];
-           UIView *bg = [[UIView alloc] initWithFrame:self.bounds];
-           [bg setBackgroundColor:UIColor.clearColor];
-           
-           
-           UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-           [button setUserInteractionEnabled:NO];
-           [button setTitle:self.deleteTitle?self.deleteTitle:@"删除" forState:UIControlStateNormal];
-           [button.titleLabel setFont:[UIFont systemFontOfSize:14]];
-           [button .titleLabel setTextColor:UIColor.whiteColor];
-           [button setFrame:CGRectMake(rect.size.width-70, supperRect.origin.y<0?(supperRect.origin.y+(supperRect.size.height-headRect.size.height-footRect.size.height)/2-44/2):((supperRect.size.height-headRect.size.height-footRect.size.height)/2-44/2), 70, 44)];
-           UIView *btnBg = [[UIView alloc] initWithFrame:CGRectMake(rect.size.width-70, 0, 70, image.size.height-1)];
-           [btnBg setBackgroundColor:UIColor.redColor];
-         
-           
-           [self addSubview:bg];
-           CGRect bgRect = [self convertRect:view.frame toView:bg];
-           [view setFrame:bgRect];
-           [imageView setFrame:bgRect];
-           [bg addSubview:view];
-           [bg addSubview:imageView];
-           [view addSubview:btnBg];
-           [view addSubview:button];
-           
-           
-          
-           self.index = [NSNumber numberWithInteger:indexPath.section];
-           self.swipeBg = bg;
-           self.swipeCellBg = view;
-           self.swipeRedBg = btnBg;
-           self.swipeImageView = imageView;
-          
-         
-           [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.75 initialSpringVelocity:1.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
-               [imageView setFrame:CGRectMake(-70, self.swipeImageView.frame.origin.y, self.swipeImageView.frame.size.width, self.swipeImageView.frame.size.height)];
-           } completion:^(BOOL finished) {
-               
-           }];
-           UITapGestureRecognizer *deltap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(delAction)];
-           [self.swipeRedBg addGestureRecognizer:deltap];
-           UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(closeAction)];
-           [self.swipeBg addGestureRecognizer:tap];
-           UISwipeGestureRecognizer * recognizer;
-           recognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(closeAction)];
-              [recognizer setDirection:(UISwipeGestureRecognizerDirectionRight)];
-              [self.swipeBg addGestureRecognizer:recognizer];
-           recognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(closeAction)];
-             [recognizer setDirection:(UISwipeGestureRecognizerDirectionLeft)];
-             [self.swipeBg addGestureRecognizer:recognizer];
-         
-       }
-      
-      
-       
-       
-   }
-   
+            UIImage *image = [self screenShotForIndexPaths:indexPath];
+            UIView *view = [[UIView alloc] init];
+            [view setBackgroundColor:self.backgroundColor];
+            UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+            [view setFrame:CGRectMake(0,rect.origin.y+headRect.size.height, rect.size.width, image.size.height)];
+            [view setClipsToBounds:YES];
+            
+            
+            
+            UIView *bg = [[UIView alloc] initWithFrame:self.bounds];
+            [bg setBackgroundColor:UIColor.clearColor];
+            
+            
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button setUserInteractionEnabled:NO];
+            [button setTitle:self.deleteTitle?self.deleteTitle:@"删除" forState:UIControlStateNormal];
+            [button.titleLabel setFont:[UIFont systemFontOfSize:14]];
+            [button .titleLabel setTextColor:UIColor.whiteColor];
+            [button setFrame:CGRectMake(rect.size.width-70, supperRect.origin.y<0?(supperRect.origin.y+(supperRect.size.height-headRect.size.height-footRect.size.height)/2-44/2):((supperRect.size.height-headRect.size.height-footRect.size.height)/2-44/2), 70, 44)];
+            UIView *btnBg = [[UIView alloc] initWithFrame:CGRectMake(rect.size.width-70, 0, 70, image.size.height)];
+            [btnBg setBackgroundColor:UIColor.redColor];
+            
+            
+            [self addSubview:bg];
+            CGRect bgRect = [self convertRect:view.frame toView:bg];
+            [view setFrame:bgRect];
+            [imageView setFrame:bgRect];
+            [bg addSubview:view];
+            [bg addSubview:imageView];
+            [view addSubview:btnBg];
+            [view addSubview:button];
+            
+            
+            
+            self.index = [NSNumber numberWithInteger:indexPath.section];
+            self.swipeBg = bg;
+            self.swipeCellBg = view;
+            self.swipeRedBg = btnBg;
+            self.swipeImageView = imageView;
+            
+            
+            [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.75 initialSpringVelocity:1.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+                [imageView setFrame:CGRectMake(-70, self.swipeImageView.frame.origin.y, self.swipeImageView.frame.size.width, self.swipeImageView.frame.size.height)];
+            } completion:^(BOOL finished) {
+                
+            }];
+            UITapGestureRecognizer *deltap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(delAction)];
+            [self.swipeRedBg addGestureRecognizer:deltap];
+            UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(closeAction)];
+            [self.swipeBg addGestureRecognizer:tap];
+            UISwipeGestureRecognizer * recognizer;
+            recognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(closeAction)];
+            [recognizer setDirection:(UISwipeGestureRecognizerDirectionRight)];
+            [self.swipeBg addGestureRecognizer:recognizer];
+            recognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(closeAction)];
+            [recognizer setDirection:(UISwipeGestureRecognizerDirectionLeft)];
+            [self.swipeBg addGestureRecognizer:recognizer];
+            
+        }
+        
+        
+        
+        
+    }
+    
 }
 -(void)closeAction{
     [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.75 initialSpringVelocity:1.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
@@ -194,13 +193,13 @@
     } completion:^(BOOL finished) {
         self.isOpen = [NSNumber numberWithInt:0];
         [self.swipeBg removeFromSuperview];
-       
+        
     }];
 }
 -(void)delAction{
     [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.75 initialSpringVelocity:1.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
         [self.swipeRedBg setAlpha:0];
-    
+        
         [self.swipeImageView setFrame:CGRectMake(0, self.swipeImageView.frame.origin.y, self.swipeImageView.frame.size.width, self.swipeImageView.frame.size.height)];
     } completion:^(BOOL finished) {
         self.isOpen = [NSNumber numberWithInt:0];
@@ -209,16 +208,17 @@
             self.callBack(self.index.integerValue);
         }
     }];
-   
+    
 }
 
 - (UIImage*)screenShotForIndexPaths:(NSIndexPath*)indexPath{
     long number = [self numberOfRowsInSection:indexPath.section];
-//    CGPoint originalOffset = self.contentOffset;
+  
     CGRect rect = [self rectForSection:indexPath.section];
     CGRect headRect = [self rectForHeaderInSection:indexPath.section];
     CGRect footRect = [self rectForFooterInSection:indexPath.section];
-    UIGraphicsBeginImageContextWithOptions((CGSizeMake(rect.size.width, rect.size.height-headRect.size.height-footRect.size.height)), NO, 0.0);
+   //增加0.00精度设置避免图片因精度转换变大
+    UIGraphicsBeginImageContextWithOptions((CGSizeMake(rect.size.width, floor((rect.size.height-headRect.size.height-footRect.size.height) * 100) / 100 )), NO, 0.0);
     
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     
@@ -235,21 +235,21 @@
     
     UIGraphicsEndImageContext();
     
-   
+    
     
     return image;
 }
 -(void)addNotification{
- 
+    
     [self addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
-   
+    
 }
 
 -(void)dealloc{
     if (self.callBack) {
         [self removeObserver:self forKeyPath:@"contentOffset"];
     }
-   
+    
 }
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
 {
@@ -259,7 +259,7 @@
                 self.isOpen = [NSNumber numberWithBool:NO];
                 [self closeAction];
             }
-           
+            
         }
     }
     
